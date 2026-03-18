c3d723f70a3c0d6d8fcaf60d8c36e2e5fbfa99cf
Diego Gil <diego.gil2505@gmail.com>
2026-03-18 09:56:13 -0300

    update nvim plugins

---
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,18 @@
+-- extend lazuvim extra
+return {
+  -- "yetone/avante.nvim",
+  -- opts = {
+  --   provider = "copilot",
+  --   providers = {
+  --     copilot = {
+  --       model = "claude-sonnet-4.5",
+  --     },
+  --     gemini = {
+  --       model = "gemini-3-pro-preview",
+  --     },
+  --   },
+  --   selector = {
+  --     exclude_auto_select = { "NvimTree" },
+  --   },
+  -- },
+}
